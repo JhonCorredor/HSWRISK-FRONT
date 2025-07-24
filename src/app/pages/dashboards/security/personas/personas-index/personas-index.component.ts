6de3de85432ac1ebd3fe4178edc88cb5f1deeef9
ingrid0208 <yulissamain.04@gmail.com>
2025-07-24 10:58:33 -0500

    modify the person table

--- src/app/pages/dashboards/security/personas/personas-index/personas-index.component.ts
+++ src/app/pages/dashboards/security/personas/personas-index/personas-index.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, NgModule, signal } from '@angular/core';
+import { Component, OnInit, NgModule, signal, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GeneralModule } from 'src/app/general/general.module';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
@@ -8,6 +8,8 @@ import { GeneralParameterService } from '../../../../../generic/general.service'
 import { DatatableParameter } from '../../../../../admin/datatable.parameters';
 import { Persona } from '../personas.module';
 import { LANGUAGE_DATATABLE } from 'src/app/admin/datatable.language';
+import { Subject } from 'rxjs';
+import { DataTableDirective } from 'angular-datatables';
 
 @Component({
   selector: 'app-personas-index',
@@ -22,6 +24,11 @@ export class PersonasIndexComponent implements OnInit {
   botones: String[] = ['btn-nuevo'];
   listPersonas = signal<Persona[]>([]);
 
+
+  public dtTrigger: Subject<any> = new Subject();
+      @ViewChild(DataTableDirective) dtElement!: DataTableDirective;
+      dtOptions: DataTables.Settings = {};
+
   constructor(
     private service: GeneralParameterService,
     private modalService: NgbModal,
@@ -34,47 +41,133 @@ export class PersonasIndexComponent implements OnInit {
     this.cargarLista();
   }
 
-  cargarLista() {
-    this.getData()
-    .then((datos) => {
-      datos.data.forEach((item: any) => {
-        this.listPersonas.update(listPersonas => {
-          const Persona: Persona = {
-            id: item.id,
-            activo: item.activo,
-            tipoDocumento: item.tipoDocumento,
-            documento: item.documento,
-            primerNombre: item.primerNombre,
-            segundoNombre: item.segundoNombre,
-            primerApellido: item.primerApellido,
-            segundoApellido: item.segundoApellido,
-            direccion: item.direccion,
-            dateBirth: item.dateBirth,
-            telefono: item.telefono,
-            email: item.email,
-            genero: item.genero,
-            countryBirth: item.countryBirth,
-            ciudad: item.ciudad,
-            ciudadId: item.ciudadId,
-          };
+  
+    cargarLista() {
+      this.dtOptions = {
+        dom: 'Blfrtip',
+        processing: true,
+        ordering: true,
+        responsive: true,
+        paging: true,
+        order: [0, 'desc'],
+        language: LANGUAGE_DATATABLE,
+        ajax: (dataTablesParameters: any, callback: any) => {
+          var data = new DatatableParameter();
+          data.pageNumber = '';
+          data.pageSize = '';
+          data.filter = '';
+          data.columnOrder = '';
+          data.directionOrder = '';
+          this.service.datatable('persona', data).subscribe((res) => {
+            callback({
+              recordsTotal: res.data.length,
+              recordsFiltered: res.data.length,
+              draw: dataTablesParameters.draw,
+              data: res.data,
+            });
+          });
+        },
+        columns: [
+          {
+            title: 'TIPO DOCUMENTO',
+            data: 'tipoDocumento',
+            className: 'text-center',
+          },
+          {
+            title: 'DOCUMENTO',
+            data: 'documento',
+            className: 'text-center',
+          },
+                    {
+            title: 'NOMBRE COMPLETO',
+            data: null,
+            className: 'text-center',
+            render: function (item: any) {
+            var convenio = `${item.primerNombre ?? ''} ${item.segundoNombre ? item.segundoNombre + ' ' : ''}${item.primerApellido ?? ''} ${item.segundoApellido ?? ''}`;
+            return convenio;
+          }
+          },
+                    {
+            title: 'DIRECCIÓN',
+            data: 'direccion',
+            className: 'text-center',
+          },
+                    {
+            title: 'TELÉFONO',
+            data: 'telefono',
+            className: 'text-center',
+          },
+                    {
+            title: 'EMAIL',
+            data: 'email',
+            className: 'text-center',
+          },
+                              {
+            title: 'GENERO',
+            data: 'genero',
+            className: 'text-center',
+          },
+                              {
+            title: 'FECHA DE NACIMIENTO',
+            data: 'dateBirth',
+            className: 'text-center',
+          },
+                              {
+            title: 'PAIS DE NACIMIENTO',
+            data: 'countryBirth',
+            className: 'text-center',
+          },
 
-          return [...listPersonas, Persona];
-        });
-      });
+          {
+            title: 'ESTADO',
+            data: 'activo',
+            className: 'text-center',
+            render: function (item: any) {
+              if (item) {
+                return "<label class='text-center text-success'>Activo</label>";
+              } else {
+                return "<label class='text-center text-danger'>Inactivo</label>";
+              }
+            },
+          },
+          {
+            title: 'ACCIONES',
+            orderable: false,
+            data: 'id',
+            className: 'text-center',
+            render: function (id: any) {
+              return `<div role="group"  class="button-group " aria-label="Basic example">
+                            <button type="button" title="Editar" class="btn btn-sm text-secondary btn-dropdown-modificar" data-id="${id}"><i class="fa-duotone fa-pen-to-square" data-id="${id}"></i> Editar</button>
+                            <button type="button" title="Eliminar" class="btn btn-sm text-secondary btn-dropdown-eliminar" data-id="${id}"><i class="fa-duotone fa-trash-can" data-id="${id}"></i> Eliminar</button>
 
-      setTimeout(() => {
-        $("#datatable").DataTable({
-          dom: 'Blfrtip',
-          destroy: true,
-          language: LANGUAGE_DATATABLE,
-          processing: true
-        });
-      }, 200);
-    })
-    .catch((error) => {
-      console.error('Error al obtener los datos:', error);
-    });
-  }
+                          </div>`;
+            },
+          },
+        ],
+        drawCallback: () => {
+          $('.btn-dropdown-modificar')
+            .off()
+            .on('click', (event: any) => {
+              this.updateGeneric(event.currentTarget.dataset.id);
+            });
+
+          $('.btn-dropdown-eliminar')
+            .off()
+            .on('click', (event: any) => {
+              this.deleteGeneric(event.currentTarget.dataset.id);
+            });
+        },
+      };
+    }
+
+
+        ngAfterViewInit() {
+      this.dtTrigger.next(this.dtOptions);
+    }
+
+    ngOnDestroy(): void {
+      this.dtTrigger.unsubscribe();
+    }
 
   getData(): Promise<any> {
     var data = new DatatableParameter(); data.pageNumber = ""; data.pageSize = ""; data.filter = ""; data.columnOrder = ""; data.directionOrder = "";
